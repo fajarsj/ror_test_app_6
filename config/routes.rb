cc0f45f71378ded23bb1b98c2476b52d99b92a89
fajarsj <fajarthea5@gmail.com>
2021-06-23 23:51:47 +0700

    Initial commit and add route path

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  # this will link to "application_controller" you dont need to write the "_controller" because rails already know,and  the "#" mean is method name inside the controller
+  root 'pages#home'
+end
