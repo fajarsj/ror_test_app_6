b8fb6d7bb36cdc843b49395ae30d7d86bb17065e
fajarsj <fajarthea5@gmail.com>
2021-07-07 02:30:44 +0700

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # this will link to "application_controller" you dont need to write the "_controller" because rails already know,and  the "#" mean is method name inside the controller
   root 'pages#home'
+  get 'about', to: "pages#about"
 end
